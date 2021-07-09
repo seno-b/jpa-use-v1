2ea880fb01369d075efc1b19955827aa2f4b2d84
seno-b <bangshinho@gmail.com>
2021-07-09 23:40:43 +0900

    [fature] : 7. 주문 도메인 개발  - 상품 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+
+}
